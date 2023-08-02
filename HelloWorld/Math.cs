8b1fcef9962db1b60324b016eb61e854be835dc2
unknown <Dell@DESKTOP-TUA23UQ>
2023-08-02 19:49:32 +0200

    Add project files.

---
+++ HelloWorld/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            double a = 20;
+            double b = 10;
+
+            Math math = new Math();
+            double addResult = math.Add(a, b);
+
+            double substractResult = math.Substract(a, b);
+
+            Console.WriteLine(addResult.ToString());
+            Console.WriteLine(substractResult.ToString());
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imię = "Dagmara";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Dagmara Bajerlein.");
+        }
+    }
+}
